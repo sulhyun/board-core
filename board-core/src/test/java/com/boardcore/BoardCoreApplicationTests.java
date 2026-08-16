50e9cc544786d6a6973749d97adb550babbd2e75
sulhyun <sulhyun4788@naver.com>
2026-08-16 17:40:34 +0900

    프로젝트 생성

---
+++ board-core/src/test/java/com/boardcore/BoardCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
