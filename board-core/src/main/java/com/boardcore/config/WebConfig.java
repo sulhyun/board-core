648c748f0683258e029042b28e1aacadaef95781
sulhyun <sulhyun4788@naver.com>
2026-08-29 23:42:00 +0900

    인터셉터

---
+++ board-core/src/main/java/com/boardcore/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.boardcore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.boardcore.interceptor.MemberInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new MemberInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns(
+				"/*.ico", "/error", "/css/**", "/js/**", "/images/**", 
+				"/", "/member/login", "/member/logout", "/member/signup"
+			);
+	}
+
+}
